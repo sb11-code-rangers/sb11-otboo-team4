fd7a9575f0b66bceb2ba175004604bb53d0e7776
Ksinny <dosly2@naver.com>
2026-07-27 19:04:49 +0900

    test(green): 피드 작성자 불일치 검증 로직 구현

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/service/FeedService.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/service/FeedService.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.otboo.domain.social.feed.service;
 
 import com.sprint.mission.otboo.domain.social.feed.dto.FeedCreateRequest;
+import com.sprint.mission.otboo.domain.social.feed.exception.FeedForbiddenException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,8 @@ public class FeedService {
 
   @Transactional
   public void create(FeedCreateRequest request, UUID currentUserId) {
+    if (!request.authorId().equals(currentUserId)) {
+      throw FeedForbiddenException.authorMismatch(currentUserId, request.authorId());
+    }
   }
 }
