c33cb04c4a6dc0dee94a2dd8f5bc2164306d865e
zrp0x0 <zrp0x0@gmail.com>
2026-07-27 23:57:48 +0900

    fix: 이메일 최대 길이 제약을 50자로 수정

--- src/main/java/com/sprint/mission/otboo/domain/authuser/user/dto/request/UserCreateRequest.java
+++ src/main/java/com/sprint/mission/otboo/domain/authuser/user/dto/request/UserCreateRequest.java
@@ -11,7 +11,7 @@ public record UserCreateRequest(
 
         @NotBlank(message = "이메일은 필수입니다.")
         @Email(message = "이메일 형식이 올바르지 않습니다.")
-        @Size(max = 255, message = "이메일은 255자 이하여야 합니다.")
+        @Size(max = 50, message = "이메일은 50자 이하여야 합니다.")
         String email,
 
         @NotBlank(message = "비밀번호는 필수입니다.")
