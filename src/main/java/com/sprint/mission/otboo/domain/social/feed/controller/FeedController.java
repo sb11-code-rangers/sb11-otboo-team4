309c5cb650b2fe0a6e27b95c58c68cea2a8988ed
Ksinny <dosly2@naver.com>
2026-07-27 21:14:55 +0900

    test(green): 피드 등록 요청 바디 검증 적용

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/controller/FeedController.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/controller/FeedController.java
@@ -4,6 +4,7 @@ import com.sprint.mission.otboo.domain.social.feed.controller.api.FeedApi;
 import com.sprint.mission.otboo.domain.social.feed.dto.FeedCreateRequest;
 import com.sprint.mission.otboo.domain.social.feed.dto.FeedDto;
 import com.sprint.mission.otboo.domain.social.feed.service.FeedService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -23,7 +24,7 @@ public class FeedController implements FeedApi {
 
   @PostMapping
   @Override
-  public ResponseEntity<FeedDto> createFeed(@RequestBody FeedCreateRequest request) {
+  public ResponseEntity<FeedDto> createFeed(@Valid @RequestBody FeedCreateRequest request) {
     log.debug("피드 등록 요청 - authorId={}", request.authorId());
     // TODO: @CurrentUserId 도입 시 request.authorId() → 인증 사용자 ID로 교체
     FeedDto created = feedService.create(request, request.authorId());
