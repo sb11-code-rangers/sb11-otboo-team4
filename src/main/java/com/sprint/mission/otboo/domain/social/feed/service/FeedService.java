3df62b7791aa95d32ca6a3c8370ff43bf0701fc8
Ksinny <dosly2@naver.com>
2026-07-27 20:38:42 +0900

    test(green): 피드 정상 등록 저장 및 FeedDto 반환 구현

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/service/FeedService.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/service/FeedService.java
@@ -2,6 +2,7 @@ package com.sprint.mission.otboo.domain.social.feed.service;
 
 import com.sprint.mission.otboo.domain.social.feed.dto.FeedCreateRequest;
 import com.sprint.mission.otboo.domain.social.feed.dto.FeedDto;
+import com.sprint.mission.otboo.domain.social.feed.entity.Feed;
 import com.sprint.mission.otboo.domain.social.feed.exception.FeedForbiddenException;
 import com.sprint.mission.otboo.domain.social.feed.mapper.FeedMapper;
 import com.sprint.mission.otboo.domain.social.feed.repository.FeedRepository;
@@ -23,6 +24,8 @@ public class FeedService {
     if (!request.authorId().equals(currentUserId)) {
       throw FeedForbiddenException.authorMismatch(currentUserId, request.authorId());
     }
-    return null;
+    Feed feed = feedRepository.save(
+        Feed.create(request.authorId(), request.weatherId(), request.content()));
+    return feedMapper.toDto(feed, false);
   }
 }
