e986f92e74b15b9490908907ce4fb39fe0e18abe
Ksinny <dosly2@naver.com>
2026-07-27 21:12:34 +0900

    test(green): 피드 등록 컨트롤러 201 응답 구현

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/controller/FeedController.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/controller/FeedController.java
@@ -4,8 +4,8 @@ import com.sprint.mission.otboo.domain.social.feed.controller.api.FeedApi;
 import com.sprint.mission.otboo.domain.social.feed.dto.FeedCreateRequest;
 import com.sprint.mission.otboo.domain.social.feed.dto.FeedDto;
 import com.sprint.mission.otboo.domain.social.feed.service.FeedService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/feeds")
 @RestController
@@ -22,7 +23,10 @@ public class FeedController implements FeedApi {
 
   @PostMapping
   @Override
-  public ResponseEntity<FeedDto> createFeed(@Valid @RequestBody FeedCreateRequest request) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(null);
+  public ResponseEntity<FeedDto> createFeed(@RequestBody FeedCreateRequest request) {
+    log.debug("피드 등록 요청 - authorId={}", request.authorId());
+    // TODO: @CurrentUserId 도입 시 request.authorId() → 인증 사용자 ID로 교체
+    FeedDto created = feedService.create(request, request.authorId());
+    return ResponseEntity.status(HttpStatus.CREATED).body(created);
   }
 }
