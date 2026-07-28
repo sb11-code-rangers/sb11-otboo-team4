d653e5b8df9f4346c8f9c0193de0610c8feadef0
zrp0x0 <zrp0x0@gmail.com>
2026-07-28 15:16:40 +0900

    refactor: AuthService 로그인 인증 로직 직접 검증 => AuthenticationManager로 변경

--- src/main/java/com/sprint/mission/otboo/domain/authuser/auth/service/AuthService.java
+++ src/main/java/com/sprint/mission/otboo/domain/authuser/auth/service/AuthService.java
@@ -5,13 +5,16 @@ import com.sprint.mission.otboo.domain.authuser.auth.dto.response.SignInDto;
 import com.sprint.mission.otboo.domain.authuser.auth.exception.AccountLockedException;
 import com.sprint.mission.otboo.domain.authuser.auth.exception.InvalidCredentialsException;
 import com.sprint.mission.otboo.domain.authuser.auth.mapper.AuthMapper;
-import com.sprint.mission.otboo.domain.authuser.user.entity.User;
-import com.sprint.mission.otboo.domain.authuser.user.repository.UserRepository;
+import com.sprint.mission.otboo.global.security.details.CustomUserDetails;
 import com.sprint.mission.otboo.global.security.jwt.JwtProvider;
 import com.sprint.mission.otboo.global.usersession.UserSession;
 import com.sprint.mission.otboo.global.usersession.UserSessionRegistry;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,51 +23,51 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class AuthService {
 
-    private final UserRepository userRepository;
     private final UserSessionRegistry userSessionRegistry;
     private final JwtProvider jwtProvider;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final AuthMapper authMapper;
 
-    @Transactional
     public SignInDto signIn(SignInRequest request) {
 
-        User foundUser = userRepository.findByEmail(request.username())
-                .orElseThrow(InvalidCredentialsException::withNone);
-
-        boolean isAuthenticated = passwordEncoder.matches(request.password(), foundUser.getPassword());
-
-        if (!isAuthenticated) {
-            throw InvalidCredentialsException.withNone();
-        }
-
-        if (foundUser.isLocked()) {
-            throw AccountLockedException.withEmail(request.username());
-        }
+        Authentication authentication  = authenticate(request);
+        CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
 
         // 로그인 성공 이후 세션 / JWT 발급
         UserSession issued = userSessionRegistry.issue();
 
         String accessToken = jwtProvider.createAccessToken(
-                foundUser.getId(),
-                foundUser.getRole().name(),
+                principal.getUserId(),
+                principal.getRole().name(),
                 issued.sessionId(),
                 issued.issuedAt()
         );
 
         String refreshToken = jwtProvider.createRefreshToken(
-                foundUser.getId(),
+                principal.getUserId(),
                 issued.sessionId(),
                 issued.currentRefreshJti(),
                 issued.issuedAt()
         );
 
-        SignInDto result = AuthMapper.signInDtoFrom(foundUser, accessToken, refreshToken);
+        SignInDto result = authMapper.signInDtoFrom(principal, accessToken, refreshToken);
 
-        /**
-         * 주의: 항상 마지막에 존재해야함: 트랜잭션 롤백 영향 최소화
-         */
-        userSessionRegistry.save(foundUser.getId(), issued, jwtProvider.getRefreshTokenExpiresAt(issued.issuedAt()));
+        userSessionRegistry.save(principal.getUserId(), issued, jwtProvider.getRefreshTokenExpiresAt(issued.issuedAt()));
 
         return result;
+    }
+
+    private Authentication authenticate(SignInRequest request) {
+        try {
+            return authenticationManager.authenticate(
+                    UsernamePasswordAuthenticationToken.unauthenticated(
+                            request.username(), request.password()
+                    )
+            );
+        } catch (LockedException e) {
+            throw AccountLockedException.withEmail(request.username());
+        } catch (AuthenticationException e) {
+            throw InvalidCredentialsException.withNone();
+        }
     }
 }
