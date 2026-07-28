ae97040b0323bbffcae670252060d1e52d9924e3
Ksinny <dosly2@naver.com>
2026-07-28 10:37:22 +0900

    fix: 피드 등록 요청 clothesIds 원소 null 검증 추가

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/dto/FeedCreateRequest.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/dto/FeedCreateRequest.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public record FeedCreateRequest(
     @NotNull UUID authorId,
     @NotNull UUID weatherId,
-    @NotEmpty List<UUID> clothesIds,
+    @NotEmpty List<@NotNull UUID> clothesIds,
     @NotBlank String content
 ) {
 
