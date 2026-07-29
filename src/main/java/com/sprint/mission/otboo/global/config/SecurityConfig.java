0426652bcc6f48cc1f351d191a0fbcbdb92fe4b9
zrp0x0 <zrp0x0@gmail.com>
2026-07-29 18:46:23 +0900

    feat: /api/auth/reset-password 인가 전체 허용 설정 추가

--- src/main/java/com/sprint/mission/otboo/global/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/otboo/global/config/SecurityConfig.java
@@ -80,6 +80,7 @@ public class SecurityConfig {
         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
         .requestMatchers(HttpMethod.POST, "/api/auth/sign-in").permitAll()
         .requestMatchers(HttpMethod.POST, "/api/auth/refresh").permitAll()
+        .requestMatchers(HttpMethod.POST, "/api/auth/reset-password").permitAll()
 
         .anyRequest().authenticated()
     );
