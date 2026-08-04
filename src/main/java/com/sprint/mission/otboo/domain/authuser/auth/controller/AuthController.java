61a1051b7b2d6298213b39903a1f45756f80b08c
zrp0x0 <zrp0x0@gmail.com>
2026-08-04 10:20:49 +0900

    test(green): Auth 관련 기능 리팩토링

--- src/main/java/com/sprint/mission/otboo/domain/authuser/auth/controller/AuthController.java
+++ src/main/java/com/sprint/mission/otboo/domain/authuser/auth/controller/AuthController.java
@@ -1,84 +1,84 @@
-//package com.sprint.mission.otboo.domain.authuser.auth.controller;
-//
-//import com.sprint.mission.otboo.domain.authuser.auth.controller.api.AuthApi;
-//import com.sprint.mission.otboo.domain.authuser.auth.dto.request.ResetPasswordRequest;
-//import com.sprint.mission.otboo.domain.authuser.auth.dto.request.SignInRequest;
-//import com.sprint.mission.otboo.domain.authuser.auth.dto.response.JwtDto;
-//import com.sprint.mission.otboo.domain.authuser.auth.dto.response.SignInDto;
-//import com.sprint.mission.otboo.domain.authuser.auth.service.AuthService;
-//import com.sprint.mission.otboo.global.security.cookie.RefreshTokenCookieProvider;
-//import com.sprint.mission.otboo.global.security.details.CurrentUser;
-//import com.sprint.mission.otboo.global.security.details.UserPrincipal;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.validation.Valid;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.web.csrf.CsrfToken;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/auth")
-//public class AuthController implements AuthApi {
-//
-//  private final AuthService authService;
-//  private final RefreshTokenCookieProvider refreshTokenCookieProvider;
-//
-//  @Override
-//  @PostMapping("/sign-out")
-//  public ResponseEntity<Void> signOut(
-//      @CurrentUser UserPrincipal principal,
-//      HttpServletResponse response
-//  ) {
-//    authService.signOut(principal.userId());
-//    refreshTokenCookieProvider.clear(response);
-//    return ResponseEntity
-//        .status(HttpStatus.NO_CONTENT)
-//        .build();
-//  }
-//
-//  @Override
-//  @PostMapping("/sign-in")
-//  public ResponseEntity<JwtDto> signIn(
-//      @Valid @ModelAttribute SignInRequest request,
-//      HttpServletResponse response
-//  ) {
-//    SignInDto result = authService.signIn(request);
-//    refreshTokenCookieProvider.attach(response, result.refreshToken());
-//    return ResponseEntity
-//        .status(HttpStatus.OK)
-//        .body(result.jwtDto());
-//  }
-//
-//  @Override
-//  @PostMapping("/reset-password")
-//  public ResponseEntity<Void> resetPassword(@Valid @RequestBody ResetPasswordRequest request) {
-//    authService.resetPassword(request);
-//    return ResponseEntity
-//        .status(HttpStatus.OK)
-//        .build();
-//  }
-//
-//  @Override
-//  @PostMapping("/refresh")
-//  public ResponseEntity<JwtDto> refresh(
-//      @CookieValue(name = RefreshTokenCookieProvider.REFRESH_TOKEN) String refreshToken,
-//      HttpServletResponse response
-//  ) {
-//    SignInDto result = authService.refresh(refreshToken);
-//    refreshTokenCookieProvider.attach(response, result.refreshToken());
-//    return ResponseEntity
-//        .status(HttpStatus.OK)
-//        .body(result.jwtDto());
-//  }
-//
-//  @Override
-//  @GetMapping("/csrf-token")
-//  public ResponseEntity<Void> csrfToken(CsrfToken csrfToken) {
-//    csrfToken.getToken();
-//    return ResponseEntity
-//        .status(HttpStatus.NO_CONTENT)
-//        .build();
-//  }
-//}
+package com.sprint.mission.otboo.domain.authuser.auth.controller;
+
+import com.sprint.mission.otboo.domain.authuser.auth.controller.api.AuthApi;
+import com.sprint.mission.otboo.domain.authuser.auth.dto.request.ResetPasswordRequest;
+import com.sprint.mission.otboo.domain.authuser.auth.dto.request.SignInRequest;
+import com.sprint.mission.otboo.domain.authuser.auth.dto.response.JwtDto;
+import com.sprint.mission.otboo.domain.authuser.auth.dto.response.SignInDto;
+import com.sprint.mission.otboo.domain.authuser.auth.service.AuthService;
+import com.sprint.mission.otboo.security.cookie.RefreshTokenCookieProvider;
+import com.sprint.mission.otboo.security.details.CurrentUser;
+import com.sprint.mission.otboo.security.details.UserPrincipal;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController implements AuthApi {
+
+  private final AuthService authService;
+  private final RefreshTokenCookieProvider refreshTokenCookieProvider;
+
+  @Override
+  @PostMapping("/sign-out")
+  public ResponseEntity<Void> signOut(
+      @CurrentUser UserPrincipal principal,
+      HttpServletResponse response
+  ) {
+    authService.signOut(principal.userId());
+    refreshTokenCookieProvider.clear(response);
+    return ResponseEntity
+        .status(HttpStatus.NO_CONTENT)
+        .build();
+  }
+
+  @Override
+  @PostMapping("/sign-in")
+  public ResponseEntity<JwtDto> signIn(
+      @Valid @ModelAttribute SignInRequest request,
+      HttpServletResponse response
+  ) {
+    SignInDto result = authService.signIn(request);
+    refreshTokenCookieProvider.attach(response, result.refreshToken());
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(result.jwtDto());
+  }
+
+  @Override
+  @PostMapping("/reset-password")
+  public ResponseEntity<Void> resetPassword(@Valid @RequestBody ResetPasswordRequest request) {
+    authService.resetPassword(request);
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .build();
+  }
+
+  @Override
+  @PostMapping("/refresh")
+  public ResponseEntity<JwtDto> refresh(
+      @CookieValue(name = RefreshTokenCookieProvider.REFRESH_TOKEN) String refreshToken,
+      HttpServletResponse response
+  ) {
+    SignInDto result = authService.refresh(refreshToken);
+    refreshTokenCookieProvider.attach(response, result.refreshToken());
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(result.jwtDto());
+  }
+
+  @Override
+  @GetMapping("/csrf-token")
+  public ResponseEntity<Void> csrfToken(CsrfToken csrfToken) {
+    csrfToken.getToken();
+    return ResponseEntity
+        .status(HttpStatus.NO_CONTENT)
+        .build();
+  }
+}
