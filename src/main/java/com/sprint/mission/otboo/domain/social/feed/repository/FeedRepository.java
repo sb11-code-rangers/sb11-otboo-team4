6474e9b87d756fe58126c246ea74faeb68af7884
Ksinny <dosly2@naver.com>
2026-08-04 16:58:57 +0900

    test(green): 피드 좋아요 카운트 증가 구현

--- src/main/java/com/sprint/mission/otboo/domain/social/feed/repository/FeedRepository.java
+++ src/main/java/com/sprint/mission/otboo/domain/social/feed/repository/FeedRepository.java
@@ -4,9 +4,13 @@ import com.sprint.mission.otboo.domain.social.feed.entity.Feed;
 import com.sprint.mission.otboo.domain.social.feed.repository.querydsl.FeedCustomRepository;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FeedRepository extends JpaRepository<Feed, UUID>, FeedCustomRepository {
 
-  default void incrementLikeCount(UUID feedId) {
-  }
+  @Modifying(clearAutomatically = true, flushAutomatically = true)
+  @Query("update Feed f set f.likeCount = f.likeCount + 1 where f.id = :feedId")
+  void incrementLikeCount(@Param("feedId") UUID feedId);
 }
