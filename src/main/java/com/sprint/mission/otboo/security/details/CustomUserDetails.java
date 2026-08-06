eeba58ba2109fac25aa165e919824fd361c5f4de
zrp0x0 <zrp0x0@gmail.com>
2026-08-06 16:36:52 +0900

    refactor: CustomUserDetails 필드 -> UserDto로 변환

--- src/main/java/com/sprint/mission/otboo/security/details/CustomUserDetails.java
+++ src/main/java/com/sprint/mission/otboo/security/details/CustomUserDetails.java
@@ -1,48 +1,31 @@
 package com.sprint.mission.otboo.security.details;
 
-import com.sprint.mission.otboo.domain.authuser.user.entity.User;
-import com.sprint.mission.otboo.domain.authuser.user.entity.enums.Role;
-import lombok.Getter;
+import com.sprint.mission.otboo.domain.authuser.user.dto.response.UserDto;
+import java.util.Collection;
+import java.util.List;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.Instant;
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
-
-@Getter
 public class CustomUserDetails implements UserDetails, CredentialsContainer {
 
-  private final UUID userId;
-  private final String email;
-  private final String name;
-  private final Role role;
-  private final boolean locked;
-  private final Instant createdAt;
+  private UserDto userDto;
   private String password;
 
-  public CustomUserDetails(User user) {
-    this.userId = user.getId();
-    this.email = user.getEmail();
-    this.name = user.getName();
-    this.role = user.getRole();
-    this.locked = user.isLocked();
-    this.createdAt = user.getCreatedAt();
-    this.password = user.getPassword();
+  public CustomUserDetails(UserDto userDto, String password) {
+    this.userDto = userDto;
+    this.password = password;
   }
 
-  @Override
-  public void eraseCredentials() {
-    this.password = null;
+  public UserDto getUserDto() {
+    return userDto;
   }
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(new SimpleGrantedAuthority(role.name()));
+    return List.of(new SimpleGrantedAuthority(userDto.role().name()));
   }
 
   @Override
@@ -52,11 +35,16 @@ public class CustomUserDetails implements UserDetails, CredentialsContainer {
 
   @Override
   public String getUsername() {
-    return email;
+    return userDto.email();
   }
 
   @Override
   public boolean isAccountNonLocked() {
-    return !locked;
+    return !userDto.locked();
+  }
+
+  @Override
+  public void eraseCredentials() {
+    this.password = null;
   }
 }
