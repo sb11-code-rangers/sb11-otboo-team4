8ee0ea7eccdb124baffb0f9bc0c5084328972fef
zrp0x0 <zrp0x0@gmail.com>
2026-08-11 18:53:19 +0900

    feat(green): FileExtensionUtils 구현

---
+++ src/main/java/com/sprint/mission/otboo/global/file/util/FileExtensionUtils.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.otboo.global.file.util;
+
+import java.util.Optional;
+import org.springframework.util.StringUtils;
+
+public final class FileExtensionUtils {
+
+  private FileExtensionUtils() {
+  }
+
+  public static Optional<String> extract(String filename) {
+    if (!StringUtils.hasText(filename)) {
+      return Optional.empty();
+    }
+
+    int dotIndex = filename.lastIndexOf('.');
+    if (dotIndex < 0 || dotIndex == filename.length() - 1) {
+      return Optional.empty();
+    }
+
+    return Optional.of(filename.substring(dotIndex + 1).toLowerCase());
+  }
+}
