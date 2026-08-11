4a2042ec226ddb5add0c915a3f2441b76e96bb9c
zrp0x0 <zrp0x0@gmail.com>
2026-08-11 19:03:00 +0900

    feat(green): FileWebConfig 구현

---
+++ src/main/java/com/sprint/mission/otboo/global/file/config/FileWebConfig.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.otboo.global.file.config;
+
+import com.sprint.mission.otboo.global.file.properties.FileProperties;
+import java.nio.file.Path;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "otboo.file.impl", havingValue = "local", matchIfMissing = true)
+public class FileWebConfig implements WebMvcConfigurer {
+
+  private final FileProperties fileProperties;
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    Path uploadPath = Path.of(fileProperties.local().uploadDir()).toAbsolutePath().normalize();
+    String location = "file:///" + uploadPath.toString().replace("\\", "/") + "/";
+
+    registry.addResourceHandler("/uploads/**")
+        .addResourceLocations(location);
+  }
+}
