76a1bcce3aada7848322fa4d794ba935e6980dc5
zrp0x0 <zrp0x0@gmail.com>
2026-08-13 17:32:30 +0900

    refactor: 사용하지 않은 어노테이션 제거

--- src/main/java/com/sprint/mission/otboo/global/mail/service/impl/GoogleSmtpMailService.java
+++ src/main/java/com/sprint/mission/otboo/global/mail/service/impl/GoogleSmtpMailService.java
@@ -4,12 +4,9 @@ import com.sprint.mission.otboo.global.mail.exception.MailSendErrorException;
 import com.sprint.mission.otboo.global.mail.service.MailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
