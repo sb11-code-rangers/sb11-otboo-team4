0a5bb3dd1fe3583b69c63cb354cbb256ada3fdfc
zrp0x0 <zrp0x0@gmail.com>
2026-08-14 14:53:49 +0900

    feat(green): concurrentPolicy가 max일 때 maxDevices 필수 검증 추가

--- src/main/java/com/sprint/mission/otboo/security/usersession/properties/UserSessionProperties.java
+++ src/main/java/com/sprint/mission/otboo/security/usersession/properties/UserSessionProperties.java
@@ -1,5 +1,9 @@
 package com.sprint.mission.otboo.security.usersession.properties;
 
+import com.sprint.mission.otboo.security.usersession.properties.enums.ConcurrentPolicyType;
+import com.sprint.mission.otboo.security.usersession.properties.enums.ExpirationPolicyType;
+import com.sprint.mission.otboo.security.usersession.properties.enums.UserSessionRegistryType;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.time.Duration;
@@ -16,7 +20,32 @@ public record UserSessionProperties(
     Duration userSessionExpiration,
 
     @Positive
-    Integer maxDevice
+    Integer maxDevices,
+
+    ExpirationPolicyType expirationPolicy,
+
+    ConcurrentPolicyType concurrentPolicy,
+
+    UserSessionRegistryType impl
 ) {
 
+  public UserSessionProperties {
+    if (expirationPolicy == null) {
+      expirationPolicy = ExpirationPolicyType.ABSOLUTE;
+    }
+    if (concurrentPolicy == null) {
+      concurrentPolicy = ConcurrentPolicyType.MULTI;
+    }
+    if (impl == null) {
+      impl = UserSessionRegistryType.REDIS;
+    }
+  }
+
+  @AssertTrue(message = "concurrentPolicy가 max이면 maxDevices가 필수입니다.")
+  private boolean isMaxDevicesValid() {
+    if (concurrentPolicy != ConcurrentPolicyType.MAX) {
+      return true;
+    }
+    return maxDevices != null && maxDevices > 0;
+  }
 }
