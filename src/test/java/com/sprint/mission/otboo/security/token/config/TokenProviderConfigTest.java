68cee51d80f7ab66b2c00e25eb4991ae0e245e5c
zrp0x0 <zrp0x0@gmail.com>
2026-08-14 18:35:22 +0900

    test: TokenProviderConfigTest에 impl 생략 시 기본값 테스트와 DisplayName 복원

--- src/test/java/com/sprint/mission/otboo/security/token/config/TokenProviderConfigTest.java
+++ src/test/java/com/sprint/mission/otboo/security/token/config/TokenProviderConfigTest.java
@@ -10,10 +10,12 @@ import com.sprint.mission.otboo.security.token.provider.impl.NimbusTokenProvider
 import java.time.Clock;
 import java.time.Duration;
 import java.util.Base64;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
+@DisplayName("TokenProviderConfig")
 class TokenProviderConfigTest {
 
   private static final String SECRET = Base64.getEncoder().encodeToString(new byte[32]);
@@ -27,9 +29,11 @@ class TokenProviderConfigTest {
   }
 
   @Nested
+  @DisplayName("impl 값이 nimbus일 때")
   class NimbusSelection {
 
     @Test
+    @DisplayName("NimbusTokenProvider 빈이 등록된다")
     void 성공_impl값이_nimbus이면_NimbusTokenProvider빈이_등록된다() {
       // given & when
       contextRunner
@@ -40,12 +44,27 @@ class TokenProviderConfigTest {
             assertThat(context.getBean(TokenProvider.class)).isInstanceOf(NimbusTokenProvider.class);
           });
     }
+
+    @Test
+    @DisplayName("impl 값이 없으면 기본값으로 등록된다")
+    void 성공_impl값이_없으면_기본값으로_NimbusTokenProvider빈이_등록된다() {
+      // given & when
+      contextRunner
+          .withBean(TokenProperties.class, () -> propertiesOf(null))
+          .run(context -> {
+            // then
+            assertThat(context).hasSingleBean(TokenProvider.class);
+            assertThat(context.getBean(TokenProvider.class)).isInstanceOf(NimbusTokenProvider.class);
+          });
+    }
   }
 
   @Nested
+  @DisplayName("impl 값이 jjwt일 때")
   class JjwtSelection {
 
     @Test
+    @DisplayName("JjwtTokenProvider 빈이 등록된다")
     void 성공_impl값이_jjwt이면_JjwtTokenProvider빈이_등록된다() {
       // given & when
       contextRunner
