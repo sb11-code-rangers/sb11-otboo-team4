11b0f9e9d22d808875104339e5ad1073d971232e
zrp0x0 <zrp0x0@gmail.com>
2026-08-17 19:57:28 +0900

    refactor: LocationRequest locationNames @NotNull 제약 조건 추가

--- src/main/java/com/sprint/mission/otboo/domain/authuser/user/dto/request/LocationRequest.java
+++ src/main/java/com/sprint/mission/otboo/domain/authuser/user/dto/request/LocationRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public record LocationRequest(
@@ -23,6 +24,7 @@ public record LocationRequest(
     @NotNull(message = "y는 필수입니다.")
     Integer y,
 
+    @NotNull(message = "locationNames는 필수입니다.")
     @Size(max = 10, message = "locationNames는 10개를 넘을 수 없습니다.")
     List<String> locationNames
 ) {
